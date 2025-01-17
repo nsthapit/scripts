3bf0682839cca8a7582c8846c13507e05134f0be
Nirat Sthapit (CLBC) <146120257+nsthapit@users.noreply.github.com>
2025-01-17 14:07:24 -0800

    Create install_dotnet.ps1

---
+++ install_dotnet.ps1
@@ -0,0 +1,18 @@
+$get_env=$args[0]
+
+write-host " Install .NET Runtime $get_env" -ForegroundColor Green
+write-host " don't close this window, it will be close automatically" -ForegroundColor red
+write-host "_________________________________________________________"
+
+# Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
+$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+    if ($ResolveWingetPath){
+           $WingetPath = $ResolveWingetPath[-1].Path
+    }
+
+$config
+# cd $wingetpathexit
+cmd.exe /c "winget.exe install Microsoft.DotNet.DesktopRuntime.$get_env --silent --accept-package-agreements --accept-source-agreements --force"
+
+write-host " Install .NET Runtime $get_env Complete" -ForegroundColor Green
+exit
