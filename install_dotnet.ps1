5625689d156403e5ab3cca2f7cc5766055c881aa
Nirat Sthapit (CLBC) <146120257+nsthapit@users.noreply.github.com>
2025-02-05 11:28:54 -0800

    Update install_dotnet.ps1

--- install_dotnet.ps1
+++ install_dotnet.ps1
@@ -4,11 +4,11 @@ write-host " Install .NET Runtime $get_env" -ForegroundColor Green
 write-host " don't close this window, it will be close automatically" -ForegroundColor red
 write-host "_________________________________________________________"
 
-# Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
-$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
-    if ($ResolveWingetPath){
-           $WingetPath = $ResolveWingetPath[-1].Path
-    }
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
+#$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+#    if ($ResolveWingetPath){
+#           $WingetPath = $ResolveWingetPath[-1].Path
+#    }
 
 $config
 # cd $wingetpathexit
